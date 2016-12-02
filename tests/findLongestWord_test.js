28da2fce8cde05a23db408d1ad92ccd8b1832cc4
Dave Harned <dharned@gmail.com>
2016-12-01 22:04:37 -0600

    complete longest word tests

--- tests/findLongestWord_test.js
+++ tests/findLongestWord_test.js
@@ -10,19 +10,41 @@ describe('findLongestWord', function() {
 
     // TEST TWO: Does it return a number?
     it('function returns a number', function() {
-        var input = "The quick brown fox jumped over the lazy dog";
-        // var expected = 6;
+        var input = 'The quick brown fox jumped over the lazy dog';
         var actual = TestFunc.findLongestWord(input);
         expect(actual).to.be.a('number');
     });
 
-    // TEST THREE: QUICK BROWN FOX
-    it('Quick Brown Fox returns 6', function() {
-        var input = "The quick brown fox jumped over the lazy dog";
+    // TEST THREE: Quick Brown Fox
+    it('The quick brown fox ... returns 6', function() {
+        var input = 'The quick brown fox jumped over the lazy dog';
         var expected = 6;
         var actual = TestFunc.findLongestWord(input);
         expect(actual).to.equal(expected);
     });
+
+    // TEST FOUR: Jedi
+    it('The force is strong with this function', function() {
+        var input = 'May the force be with you';
+        var expected = 5;
+        var actual = TestFunc.findLongestWord(input);
+        expect(actual).to.equal(expected);
+    });
     
+    // TEST FIVE: Monty Python
+    it('African or European?', function() {
+        var input = 'What is the average airspeed velocity of an unladen swallow';
+        var expected = 8;
+        var actual = TestFunc.findLongestWord(input);
+        expect(actual).to.equal(expected);
+    });
+
+     // TEST SIX: Super-Long
+    it('Otorhino-what??', function() {
+        var input = 'What if we try a super-long word such as otorhinolaryngology';
+        var expected = 19;
+        var actual = TestFunc.findLongestWord(input);
+        expect(actual).to.equal(expected);
+    });   
 
 });
