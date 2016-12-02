5105f7a7cbe4fa6a8dd91c23e1b4b0cdf7fde99b
Dave Harned <dharned@gmail.com>
2016-12-01 21:49:19 -0600

    editing tests

--- tests/findLongestWord_test.js
+++ tests/findLongestWord_test.js
@@ -3,12 +3,20 @@ var TestFunc = require('../scripts/findLongestWord.js');
 
 describe('findLongestWord', function() {
 
-    // TEST ONE: IS THE FUNCTION DEFINED?
+    // TEST ONE: Is the function defined?
     it('function exists', function() {
         expect(TestFunc.findLongestWord).to.not.be.undefined;
     });
 
-    // TEST TWO: QUICK BROWN FOX
+    // TEST TWO: Does it return a number?
+    it('function returns a number', function() {
+        var input = "The quick brown fox jumped over the lazy dog";
+        // var expected = 6;
+        var actual = TestFunc.findLongestWord(input);
+        expect(actual).to.be.a('number');
+    });
+
+    // TEST THREE: QUICK BROWN FOX
     it('Quick Brown Fox returns 6', function() {
         var input = "The quick brown fox jumped over the lazy dog";
         var expected = 6;
